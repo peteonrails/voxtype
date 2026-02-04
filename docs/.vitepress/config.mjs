13ef4530924db35f5c2df2e08411c8987b680729
radiorambo <umeshisahuman@gmail.com>
2026-02-04 19:27:45 +0530

    chore(docs): add VitePress documentation initial setup

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Voxtype",
+  description: "Voice-to-text with push-to-talk for Wayland compositors.",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+  }
+})
